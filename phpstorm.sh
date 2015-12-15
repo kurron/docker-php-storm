910d90f7737dd52ae77746069f3dc28fff5ee2c3
Ron Kurr <kurr@kurron.org>
2015-12-15 22:11:30 +0000

    Fixed the launch script

--- phpstorm.sh
+++ phpstorm.sh
@@ -2,13 +2,13 @@
 
 CMD="docker run \
        --rm \
-       --name intellij \
+       --name php \
        --net "host" \
        --env DISPLAY=unix$DISPLAY \
        --user 1000:1000 \
        --volume /tmp/.X11-unix:/tmp/.X11-unix \
        --volume $HOME:/home/developer \
-       kurron/docker-intellij:latest"
+       kurron/docker-php-storm:latest"
 
 echo $CMD
 $CMD
